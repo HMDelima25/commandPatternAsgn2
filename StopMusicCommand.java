0e8921001584b0b26499047b635606f26b80da69
Honey Mae Delima <honeymaedelima@gmail.com>
2026-09-06 00:16:44 +0800

    Create StopMusicCommand.java

---
+++ StopMusicCommand.java
@@ -0,0 +1,13 @@
+public class StopMusicCommand implements Command {
+
+    private MusicPlayer musicPlayer;
+
+    public StopMusicCommand(MusicPlayer musicPlayer) {
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public void execute() {
+        musicPlayer.stop();
+    }
+}
